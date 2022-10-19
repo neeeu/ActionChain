f7def317cc57da18ff9ecf5774ed0d3945c4d679
Ingo Randolf <ingo.randolf@neu.io>
2022-10-19 12:33:39 +0200

    add Samples~

---
+++ Samples~/ActionChainSample/Log.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace NEEEU.ActionChain.Sample
+{
+    public class Log : MonoBehaviour
+    {
+        public static void debug(string text)
+        {
+            Debug.Log(text);
+        }
+
+        public static void d(int num)
+        {
+            debug("" + num);
+        }
+
+        public static void d(string text)
+        {
+            debug(text);
+        }
+
+        public static void error(string text)
+        {
+            Debug.LogError(text);
+        }
+    
+        public static void e(string text)
+        {
+            error(text);
+        }
+    }
+}
+
